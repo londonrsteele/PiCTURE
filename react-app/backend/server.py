cb8efb89042d220953f8bb4101c0df278c2ab1b0
londonrsteele <londonrsteele@gmail.com>
2024-08-25 11:35:17 -0500

    initial weather api call

--- react-app/backend/server.py
+++ react-app/backend/server.py
@@ -1,19 +1,26 @@
 # Filename : server.py
 # Author   : LR Steele
 # Descrptn : Flask server for React app
-import json
+#########################################################################################
+# Flask imports
 from flask import Flask, jsonify
 import datetime
 import subprocess
 import os.path
 import requests
-
+# Google Calendar imports
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+# Weather imports
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+import pandas as pd
+
 
 #########################################################################################
 # Initialize app
@@ -168,6 +175,112 @@ def give_calendar():
 
     except HttpError as error:
         print(f"An error occurred: {error}")
+
+#########################################################################################
+# One route for api - /weather
+#########################################################################################
+@app.route("/weather")
+def give_weather():
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+	    "latitude": 31.1171,
+	    "longitude": -97.7278,
+	    "current": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "is_day", "precipitation", "rain", "weather_code", "cloud_cover", "wind_speed_10m"],
+	    "hourly": ["relative_humidity_2m", "apparent_temperature", "weather_code", "surface_pressure", "uv_index", "is_day"],
+	    "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "sunrise", "sunset", "daylight_duration"],
+	    "temperature_unit": "fahrenheit",
+	    "wind_speed_unit": "mph",
+	    "precipitation_unit": "inch",
+	    "timezone": "America/Chicago"
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Current values. The order of variables needs to be the same as requested.
+    current = response.Current()
+    current_temperature_2m = current.Variables(0).Value()
+    current_relative_humidity_2m = current.Variables(1).Value()
+    current_apparent_temperature = current.Variables(2).Value()
+    current_is_day = current.Variables(3).Value()
+    current_precipitation = current.Variables(4).Value()
+    current_rain = current.Variables(5).Value()
+    current_weather_code = current.Variables(6).Value()
+    current_cloud_cover = current.Variables(7).Value()
+    current_wind_speed_10m = current.Variables(8).Value()
+
+    print(f"Current time {current.Time()}")
+    print(f"Current temperature_2m {current_temperature_2m}")
+    print(f"Current relative_humidity_2m {current_relative_humidity_2m}")
+    print(f"Current apparent_temperature {current_apparent_temperature}")
+    print(f"Current is_day {current_is_day}")
+    print(f"Current precipitation {current_precipitation}")
+    print(f"Current rain {current_rain}")
+    print(f"Current weather_code {current_weather_code}")
+    print(f"Current cloud_cover {current_cloud_cover}")
+    print(f"Current wind_speed_10m {current_wind_speed_10m}")
+
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_relative_humidity_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_apparent_temperature = hourly.Variables(1).ValuesAsNumpy()
+    hourly_weather_code = hourly.Variables(2).ValuesAsNumpy()
+    hourly_surface_pressure = hourly.Variables(3).ValuesAsNumpy()
+    hourly_uv_index = hourly.Variables(4).ValuesAsNumpy()
+    hourly_is_day = hourly.Variables(5).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+	    start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	    end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = hourly.Interval()),
+	    inclusive = "left"
+    )}
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+    hourly_data["apparent_temperature"] = hourly_apparent_temperature
+    hourly_data["weather_code"] = hourly_weather_code
+    hourly_data["surface_pressure"] = hourly_surface_pressure
+    hourly_data["uv_index"] = hourly_uv_index
+    hourly_data["is_day"] = hourly_is_day
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+    print(hourly_dataframe)
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_weather_code = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_max = daily.Variables(1).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(2).ValuesAsNumpy()
+    daily_sunrise = daily.Variables(3).ValuesAsNumpy()
+    daily_sunset = daily.Variables(4).ValuesAsNumpy()
+    daily_daylight_duration = daily.Variables(5).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+	    start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	    end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = daily.Interval()),
+	    inclusive = "left"
+    )}
+    daily_data["weather_code"] = daily_weather_code
+    daily_data["temperature_2m_max"] = daily_temperature_2m_max
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+    daily_data["sunrise"] = daily_sunrise
+    daily_data["sunset"] = daily_sunset
+    daily_data["daylight_duration"] = daily_daylight_duration
+
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    print(daily_dataframe)
 
 
 #########################################################################################
